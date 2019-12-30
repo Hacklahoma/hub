11b32fdccb665a8b4b4f41966d554ab5846e1e6d
s-fletcher <sfletchertx@gmail.com>
2019-12-29 19:44:15 -0600

    soft implementation of archive

--- src/components/Archive/Item/index.js
+++ src/components/Archive/Item/index.js
@@ -5,7 +5,12 @@ class Item extends React.Component {
     render() {
         return(
             <div className="Item">
-                Item
+                <div className="container">
+                    <h1>{this.props.year}</h1>
+                    <div style={{background: this.props.color}} className="card">
+                        <img src={require('../../../images/logo' + this.props.year + '.png')} alt={this.props.year + ' logo'} />
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/Archive/index.js
+++ src/components/Archive/index.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './index.scss'
+import Item from './Item'
 
 class Archive extends React.Component {
     render() {
         return(
             <div className="Archive">
-                Archive
+                <Item year="2020" color="#B8DCEC" />
+                <Item year="2019" color="#FFEED0" />
+                <Item year="2018" color="#333333" />
             </div>
         );
     }
