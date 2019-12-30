9e2fc49b2713111a9fa2786955ca3fada36cabac
s-fletcher <sfletchertx@gmail.com>
2019-12-30 14:57:50 -0600

    connected archive with json

--- src/components/Archive/index.js
+++ src/components/Archive/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './index.scss'
 import Item from './Item'
+import archive from '../../archive';
 
 class Archive extends React.Component {
     render() {
         return(
             <div className="Archive">
-                <Item year="2020" color="#B8DCEC" />
-                <Item year="2019" color="#FFEED0" />
-                <Item year="2018" color="#333333" />
+                {Object.keys(archive).reverse().map((key) =>
+                    <Item 
+                        year={key} 
+                        color={archive[key].color}
+                        devpost={archive[key].devpost}
+                        team={archive[key].team}
+                        key={key} />
+                )}
             </div>
         );
     }
