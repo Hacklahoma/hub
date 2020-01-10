27fef1a3902c955f301e0c31c177df9d11960fbd
s-fletcher <sfletchertx@gmail.com>
2020-01-10 14:51:39 -0600

    fun animations for header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,9 +9,15 @@ class Header extends React.Component {
                 <Navbar />
                 <div className="content">
                     <div className="cloudContent">
-                        <div className="cloud"></div>
-                        <div className="cloud"></div>
-                        <div className="cloud"></div>
+                        <div className="cloudFade">
+                            <div className="cloud"></div>
+                        </div>
+                        <div className="cloudFade">
+                            <div className="cloud"></div>
+                        </div>
+                        <div className="cloudFade">
+                            <div className="cloud"></div>
+                        </div>
                     </div>
                     <div className="brandContent">
                         <img src={require('../../images/flyingLogo.png')} alt="logo" />
