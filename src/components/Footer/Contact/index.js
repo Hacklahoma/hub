4c765be30277287797741c5aabdc2463e4dd9acc
s-fletcher <sfletchertx@gmail.com>
2020-03-06 15:13:16 -0600

    Removed sponsor packet

--- src/components/Footer/Contact/index.js
+++ src/components/Footer/Contact/index.js
@@ -6,7 +6,9 @@ class Contact extends React.Component {
         return(
             <div className="Contact">
                 <h1>Contact Our Team</h1>
-                <p>We'd love to hear from you! Contact us at <a href="mailto:team@hacklahoma.org">team@hacklahoma.org</a> or through social media. If you plan on sponsoring us, please visit our <a href="sponsorPacket.pdf" target="_blank">Sponsor Packet</a>. We look forward to hearing from you!</p>
+                <p>We'd love to hear from you! Contact us at <a href="mailto:team@hacklahoma.org">team@hacklahoma.org</a> or through social media.
+                {/* If you plan on sponsoring us, please visit our <a href="sponsorPacket.pdf" target="_blank">Sponsor Packet</a>. */}
+                We look forward to hearing from you!</p>
                 <div className="social">
                     <a href="https://www.facebook.com/Hacklahoma/" rel="noopener noreferrer" target="_blank"><img src={require('../../../images/facebook.svg')} alt="facebook" /></a>
                     <a href="https://twitter.com/hacklahoma/" rel="noopener noreferrer" target="_blank"><img src={require('../../../images/twitter.svg')} alt="twitter" /></a>
