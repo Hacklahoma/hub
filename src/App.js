b291fe89e2631d97c258935845f2c8dbeffa3932
s-fletcher <sfletchertx@gmail.com>
2020-04-06 22:05:03 -0500

    Transition to styled components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,30 @@
-import React from 'react'
-import './App.scss'
-import Header from './components/Header'
-import About from './components/About'
-import Archive from './components/Archive'
-import Footer from './components/Footer'
-import Loading from './components/Loading'
+import React from "react";
+import Header from "./components/Header";
+import About from "./components/About";
+import Archive from "./components/Archive";
+import Footer from "./components/Footer";
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+    text-align: center;
+`;
 
 /**
  * Controls main viewport of webapp
  */
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      loading: true // Sets default state to display loading screen
-    }
-  }
-
-  // Component mounted, remove loading screen
-  componentDidMount() {
-    this.setState({
-      loading: false
-    })
-  }
-  
-  render() {
-    if(this.state.loading)
-      return (<Loading />);
-    else
-      return (  
-        <div className="App">
-          {/* Renders header */}
-          <Header />
-          {/* Renders about section */}
-          <About />
-          {/* Renders archive items */}
-          <Archive />
-          {/* Renders footer w/ contact section */}
-          <Footer />
-        </div>
+function App() {
+    return (
+        <StyledApp>
+            {/* Renders header */}
+            <Header />
+            {/* Renders about section */}
+            <About />
+            {/* Renders archive items */}
+            <Archive />
+            {/* Renders footer w/ contact section */}
+            <Footer />
+        </StyledApp>
     );
-}
 }
 
 export default App;
